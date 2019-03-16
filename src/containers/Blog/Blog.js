addf5700bdaa3deeaed6a0c468476a34e43c8e53
Shawn <shawnantonucci2018@gmail.com>
2019-03-15 18:19:06 -0700

    conditionally rendering a full post after clicking

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import './Blog.css';
 
 class Blog extends Component {
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
 
     componentDidMount() {
@@ -28,9 +29,18 @@ class Blog extends Component {
             });
     };
 
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    };
+
     render () {
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} author={post.author} />
+            return <Post 
+                key={post.id} 
+                title={post.title}
+                author={post.author} 
+                clicked={() => this.postSelectedHandler(post.id)}
+                />
         });
         return (
             <div>
@@ -38,7 +48,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
